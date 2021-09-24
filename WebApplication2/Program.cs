65ee5ada1e8ea681e2b4f321dd41d9a12343cdc9
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2021-09-24 15:40:52 +0400

    ewfew

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -20,7 +20,7 @@ namespace WebApplication2
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://*.1000");
+                    webBuilder.UseUrls("http://*:1000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
