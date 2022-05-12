4c6ddf32204c0b2b4358b61676c03776820b871c
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2022-05-12 20:04:32 +0400

    fh

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -7,6 +7,7 @@ namespace WebApplication2.Models
         public string RequestId2 { get; set; }
         public string RequestId22 { get; set; }
         public string RequestId222 { get; set; }
+        public string RequestId2522 { get; set; }
 
     }
 }
