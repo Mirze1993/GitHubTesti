96e11d3af7b5ff9a9705e02200466df6031130f8
Mirze1993 <58223139+Mirze1993@users.noreply.github.com>
2022-05-12 19:39:02 +0400

    Update ErrorViewModel.cs

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace WebApplication2.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string RequestId1 { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
