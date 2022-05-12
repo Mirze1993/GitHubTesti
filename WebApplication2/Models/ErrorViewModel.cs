adb1a6d836df6457d04ea4d502711ed5a9eafae3
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2022-05-12 19:49:48 +0400

    rtgh

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace WebApplication2.Models
     public class ErrorViewModel
     {
         public string RequestId2 { get; set; }
+        public string RequestId22 { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
