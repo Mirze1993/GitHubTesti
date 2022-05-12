0e990b65368cb13cbff8145d8ac61c9216457446
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2022-05-12 19:53:43 +0400

    yuk

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -6,6 +6,7 @@ namespace WebApplication2.Models
     {
         public string RequestId2 { get; set; }
         public string RequestId22 { get; set; }
+        public string RequestId222 { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
