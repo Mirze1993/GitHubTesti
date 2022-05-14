a35444c9527e3760029648ff304b474856374eea
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2022-05-14 20:50:11 +0400

    vs1

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -6,8 +6,6 @@ namespace WebApplication2.Models
     {
         public string RequestId2 { get; set; }
         public string RequestId22 { get; set; }
-        public string RequestId222 { get; set; }
-        public string RequestId2522 { get; set; }
 
     }
 }
