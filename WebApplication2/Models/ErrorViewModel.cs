cdf45a6baa1b8c5132a8c45e5204becc70f80a0a
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2022-05-15 11:46:20 +0400

    vs6

--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -7,11 +7,5 @@ namespace WebApplication2.Models
         public bool ShowRequestId { get; set; }
         public bool ShowRequestId1 { get; set; }
 
-        public bool ShowRequestId4 { get; set; }
-        public bool ShowRequestId2 { get; set; }
-
-        public bool ShowRequestId3 { get; set; }
-        public bool ShowRequestId5 { get; set; }
-        public bool ShowRequestId7 { get; set; }
     }
 }
