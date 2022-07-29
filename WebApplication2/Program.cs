d52165b94d5a088c070e41dc84ff772925ebd3c5
m.c.abbasaliyev <m.c.abbasaliyev@gmail.com>
2022-07-29 17:20:22 +0400

    ddd

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication2
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //aaaaaaa
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
